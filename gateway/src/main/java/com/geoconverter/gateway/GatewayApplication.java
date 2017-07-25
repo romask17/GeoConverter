496c2c2a4cbe73f2c8103cfd81a79a8d50d7f676
Roman S.A <mail@safeone.ru>
2017-07-25 22:49:25 +0300

    add infrastructure services: monitoring and registry

--- gateway/src/main/java/com/geoconverter/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/geoconverter/gateway/GatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-//@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
